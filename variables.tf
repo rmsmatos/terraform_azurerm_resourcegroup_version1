1ae0e8eeec20d3fad8c3ec21a08b59f0d3ab1d6f
Rui Matos <rmsmatos@gmail.com>
2023-08-14 11:40:13 +0100

    Add description for tags on variables.tf

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "location" {
 
 variable "tags" {
   type        = map(string)
-  description = "(Required) The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
+  description = "(Optional) A mapping of tags which should be assigned to the Resource Group."
   default     = null
 }
 
