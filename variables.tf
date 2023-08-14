62a00647da35e311b8d2a6ab58e24f7a137df9fc
Rui Matos <rmsmatos@gmail.com>
2023-08-14 11:42:46 +0100

    Changed variable type for managed_by from map of strings to string

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "tags" {
 }
 
 variable "managed_by" {
-  type        = map(string)
+  type        = string
   description = "(Required) The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
   default     = null
 }
